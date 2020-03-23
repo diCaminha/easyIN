97d5927294ecb18af9803136f1df0d14451b3ce5
denis <dineresc@gmail.com>
2020-03-23 19:00:54 -0300

    mudando tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easyin/screens/auth_screen.dart';
 import 'package:easyin/screens/home_condominio_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         accentColor: Colors.white
       ),
-      home: HomeCondominioScreen(), 
+      home: AuthScreen(), 
       routes: {
         
       },
